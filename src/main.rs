39084875ac286f7a66bca79f8135af590fef2347
Johnny_Ma <johnny880319@gmail.com>
2026-04-06 02:48:22 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
+        // Hi
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
