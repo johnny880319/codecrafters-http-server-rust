e4217b17b564a1f0ec03ac1c0fe7ae32d83f704c
Johnny_Ma <johnny880319@gmail.com>
2026-04-06 13:06:15 +0800

    codecrafters submit [skip ci]

--- src/repl.rs
+++ src/repl.rs
@@ -9,31 +9,13 @@ pub fn repl(stream: &mut TcpStream, dir_path: String) -> Result<()> {
     loop {
         let mut buf = [0; 4096];
         let n = stream.read(&mut buf)?;
-
         if n == 0 {
             println!("connection closed");
             break;
         }
 
-        let request_str = String::from_utf8_lossy(&buf[..n]);
-        let request_contents = request_str.split("\r\n").collect::<Vec<_>>();
-
-        println!("received request:");
-        for line in &request_contents {
-            println!("  {}", line);
-        }
-
-        let request_line = request_contents[0];
-        let request_method = request_line
-            .split(" ")
-            .next()
-            .ok_or_else(|| anyhow::anyhow!("missing request method"))?;
-        let request_target = request_line
-            .split(" ")
-            .nth(1)
-            .ok_or_else(|| anyhow::anyhow!("missing request target"))?;
-        let request_headers = &request_contents[1..request_contents.len() - 2];
-        let request_body = request_contents.last().unwrap_or(&"");
+        let (request_method, request_target, request_headers, request_body) =
+            parse_request(&buf, n)?;
 
         if request_target == "/" {
             stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
@@ -97,4 +79,37 @@ pub fn repl(stream: &mut TcpStream, dir_path: String) -> Result<()> {
         }
     }
     Ok(())
+}
+
+fn parse_request(buf: &[u8], n: usize) -> Result<(String, String, Vec<String>, String)> {
+    let request_str = String::from_utf8_lossy(&buf[..n]);
+    let request_contents = request_str.split("\r\n").collect::<Vec<_>>();
+
+    println!("received request:");
+    for line in &request_contents {
+        println!("  {}", line);
+    }
+
+    let request_line = request_contents[0];
+    let request_method = request_line
+        .split(" ")
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("missing request method"))?
+        .to_string();
+    let request_target = request_line
+        .split(" ")
+        .nth(1)
+        .ok_or_else(|| anyhow::anyhow!("missing request target"))?
+        .to_string();
+    let request_headers: Vec<String> = request_contents[1..request_contents.len() - 2]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+    let request_body = request_contents.last().unwrap_or(&"").to_string();
+    Ok((
+        request_method.to_string(),
+        request_target.to_string(),
+        request_headers,
+        request_body.to_string(),
+    ))
 }
