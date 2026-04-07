5281fe9db123b070d20af6cbf83add29f40f36bc
chounan_ma <55181887+johnny880319@users.noreply.github.com>
2026-04-07 13:48:12 +0800

    Add persistent connections and refactor response handling (#2)
    * codecrafters submit [skip ci]
    
    * codecrafters submit [skip ci]
    
    * codecrafters submit [skip ci]

--- src/command.rs
+++ src/command.rs
@@ -3,75 +3,128 @@ use crate::template;
 use anyhow::{Result, bail};
 use std::{io::Write as _, net::TcpStream};
 
+struct HttpResponse {
+    status_line: String,
+    headers: Vec<String>,
+    body: String,
+}
+
 pub fn execute_command(
     parsed_request: ParsedRequest,
     stream: &mut TcpStream,
     dir_path: String,
-) -> Result<()> {
+) -> Result<bool> {
     let request_method = parsed_request.method.as_str();
     let request_target = parsed_request.target.as_str();
+    let connection_closed = check_connection_closed(&parsed_request);
+
+    let mut response;
     match (request_method, request_target) {
-        ("GET", "/") => get_cmd(stream),
-        ("GET", target) if target.starts_with("/echo/") => get_echo(parsed_request, stream),
-        ("GET", "/user-agent") => get_user_agent(parsed_request, stream),
+        ("GET", "/") => {
+            response = get_cmd()?;
+        }
+        ("GET", target) if target.starts_with("/echo/") => {
+            response = get_echo(parsed_request)?;
+        }
+        ("GET", "/user-agent") => {
+            response = get_user_agent(parsed_request)?;
+        }
         ("GET", target) if target.starts_with("/files/") => {
-            get_files(parsed_request, stream, dir_path)
+            response = get_files(parsed_request, dir_path)?;
         }
         ("POST", target) if target.starts_with("/files/") => {
-            post_files(parsed_request, stream, dir_path)
+            response = post_files(parsed_request, dir_path)?;
         }
-        _ => not_found(stream),
+        _ => {
+            response = not_found()?;
+        }
     }
+
+    if connection_closed {
+        response.headers.push("Connection: close\r\n".to_string());
+    };
+
+    send_response(stream, response)?;
+
+    Ok(connection_closed)
 }
 
-fn get_cmd(stream: &mut TcpStream) -> Result<()> {
-    let mut response = String::new();
-    response.push_str(template::STATUS_200);
-    response.push_str(template::HEADER_END);
+fn check_connection_closed(request: &ParsedRequest) -> bool {
+    for header in &request.headers {
+        if header == "Connection: close" {
+            return true;
+        }
+    }
+    false
+}
 
-    stream.write_all(response.as_bytes())?;
+fn send_response(stream: &mut TcpStream, response: HttpResponse) -> Result<()> {
+    let mut response_str = response.status_line;
+    for header in response.headers {
+        response_str.push_str(&header);
+    }
+    response_str.push_str(template::HEADER_END);
+    response_str.push_str(&response.body);
+
+    stream.write_all(response_str.as_bytes())?;
     Ok(())
 }
 
-fn get_echo(parsed_request: ParsedRequest, stream: &mut TcpStream) -> Result<()> {
-    if let Some(echo_content) = parsed_request.target.strip_prefix("/echo/") {
-        let mut response = String::new();
-        response.push_str(template::STATUS_200);
-        response.push_str(template::CONTENT_TYPE_PLAIN);
-        response.push_str(&template::content_length(echo_content.len()));
-        response.push_str(template::HEADER_END);
-        response.push_str(echo_content);
+fn get_cmd() -> Result<HttpResponse> {
+    let status_line = template::STATUS_200.to_string();
+    let headers: Vec<String> = vec![];
+    let body = "".to_string();
 
-        stream.write_all(response.as_bytes())?;
-        return Ok(());
+    Ok(HttpResponse {
+        status_line,
+        headers,
+        body,
+    })
+}
+
+fn get_echo(parsed_request: ParsedRequest) -> Result<HttpResponse> {
+    let status_line = template::STATUS_200.to_string();
+    let mut headers = vec![template::CONTENT_TYPE_PLAIN.to_string()];
+    let body;
+
+    if let Some(echo_content) = parsed_request.target.strip_prefix("/echo/") {
+        headers.push(template::content_length(echo_content.len()));
+        body = echo_content.to_string();
+
+        return Ok(HttpResponse {
+            status_line,
+            headers,
+            body,
+        });
     }
     bail!("invalid echo request");
 }
 
-fn get_user_agent(parsed_request: ParsedRequest, stream: &mut TcpStream) -> Result<()> {
-    let mut echo_content = String::new();
+fn get_user_agent(parsed_request: ParsedRequest) -> Result<HttpResponse> {
+    let status_line = template::STATUS_200.to_string();
+    let mut headers = vec![template::CONTENT_TYPE_PLAIN.to_string()];
+
+    let mut user_agent = String::new();
     for header in parsed_request.headers {
-        if let Some(user_agent) = header.strip_prefix("User-Agent: ") {
-            echo_content = user_agent.to_string();
+        if let Some(ua) = header.strip_prefix("User-Agent: ") {
+            user_agent = ua.to_string();
             break;
         }
     }
-    let mut response = String::new();
-    response.push_str(template::STATUS_200);
-    response.push_str(template::CONTENT_TYPE_PLAIN);
-    response.push_str(&template::content_length(echo_content.len()));
-    response.push_str(template::HEADER_END);
-    response.push_str(&echo_content);
+    headers.push(template::content_length(user_agent.len()));
+    let body = user_agent;
 
-    stream.write_all(response.as_bytes())?;
-    Ok(())
+    Ok(HttpResponse {
+        status_line,
+        headers,
+        body,
+    })
 }
 
-fn get_files(
-    parsed_request: ParsedRequest,
-    stream: &mut TcpStream,
-    dir_path: String,
-) -> Result<()> {
+fn get_files(parsed_request: ParsedRequest, dir_path: String) -> Result<HttpResponse> {
+    let status_line = template::STATUS_200.to_string();
+    let mut headers = vec![template::CONTENT_TYPE_OCTET_STREAM.to_string()];
+
     let mut file_bytes = Vec::new();
     if let Some(file_path) = parsed_request.target.strip_prefix("/files/") {
         let full_path = format!("{}/{}", dir_path, file_path);
@@ -79,51 +132,47 @@ fn get_files(
         match std::fs::read(&full_path) {
             Ok(bytes) => file_bytes = bytes,
             Err(_) => {
-                let mut response = String::new();
-                response.push_str(template::STATUS_404);
-                response.push_str(template::HEADER_END);
-
-                stream.write_all(response.as_bytes())?;
-                return Ok(());
+                return not_found();
             }
         }
     }
 
-    let mut response = String::new();
-    response.push_str(template::STATUS_200);
-    response.push_str(template::CONTENT_TYPE_OCTET_STREAM);
-    response.push_str(&template::content_length(file_bytes.len()));
-    response.push_str(template::HEADER_END);
-    response.push_str(&String::from_utf8_lossy(&file_bytes));
+    headers.push(template::content_length(file_bytes.len()));
+    let body = String::from_utf8_lossy(&file_bytes).to_string();
 
-    stream.write_all(response.as_bytes())?;
-    Ok(())
+    Ok(HttpResponse {
+        status_line,
+        headers,
+        body,
+    })
 }
 
-fn post_files(
-    parsed_request: ParsedRequest,
-    stream: &mut TcpStream,
-    dir_path: String,
-) -> Result<()> {
+fn post_files(parsed_request: ParsedRequest, dir_path: String) -> Result<HttpResponse> {
+    let status_line = template::STATUS_201.to_string();
+    let headers: Vec<String> = vec![];
+    let body = "".to_string();
+
     if let Some(file_path) = parsed_request.target.strip_prefix("/files/") {
         let full_path = format!("{}/{}", dir_path, file_path);
         println!("serving file: {}", full_path);
         std::fs::write(&full_path, parsed_request.body)?;
     }
 
-    let mut response = String::new();
-    response.push_str(template::STATUS_201);
-    response.push_str(template::HEADER_END);
-
-    stream.write_all(response.as_bytes())?;
-    Ok(())
+    Ok(HttpResponse {
+        status_line,
+        headers,
+        body,
+    })
 }
 
-fn not_found(stream: &mut TcpStream) -> Result<()> {
-    let mut response = String::new();
-    response.push_str(template::STATUS_404);
-    response.push_str(template::HEADER_END);
+fn not_found() -> Result<HttpResponse> {
+    let status_line = template::STATUS_404.to_string();
+    let headers: Vec<String> = vec![];
+    let body = "".to_string();
 
-    stream.write_all(response.as_bytes())?;
-    Ok(())
+    Ok(HttpResponse {
+        status_line,
+        headers,
+        body,
+    })
 }
--- src/repl.rs
+++ src/repl.rs
@@ -1,20 +1,21 @@
 use crate::command;
 use anyhow::Result;
-use std::{io::Read as _, net::TcpStream};
+use std::{io::Read as _, net::Shutdown::Both, net::TcpStream};
 
 pub fn repl(stream: &mut TcpStream, dir_path: String) -> Result<()> {
     println!("accepted new connection");
     loop {
         let mut buf = [0; 4096];
         let n = stream.read(&mut buf)?;
-        if n == 0 {
-            println!("connection closed");
-            break;
-        }
 
         let parsed_request = parse_request(&buf, n)?;
 
-        command::execute_command(parsed_request, stream, dir_path.clone())?;
+        let connection_closed = command::execute_command(parsed_request, stream, dir_path.clone())?;
+        if connection_closed {
+            println!("connection closed by client");
+            stream.shutdown(Both)?;
+            break;
+        }
     }
     Ok(())
 }
@@ -36,25 +37,25 @@ fn parse_request(buf: &[u8], n: usize) -> Result<ParsedRequest> {
     }
 
     let request_line = request_contents[0];
-    let request_method = request_line
+    let method = request_line
         .split(" ")
         .next()
         .ok_or_else(|| anyhow::anyhow!("missing request method"))?
         .to_string();
-    let request_target = request_line
+    let target = request_line
         .split(" ")
         .nth(1)
         .ok_or_else(|| anyhow::anyhow!("missing request target"))?
         .to_string();
-    let request_headers: Vec<String> = request_contents[1..request_contents.len() - 2]
+    let headers: Vec<String> = request_contents[1..request_contents.len() - 2]
         .iter()
         .map(|s| s.to_string())
         .collect();
-    let request_body = request_contents.last().unwrap_or(&"").to_string();
+    let body = request_contents.last().unwrap_or(&"").to_string();
     Ok(ParsedRequest {
-        method: request_method.to_string(),
-        target: request_target.to_string(),
-        headers: request_headers,
-        body: request_body.to_string(),
+        method,
+        target,
+        headers,
+        body,
     })
 }
